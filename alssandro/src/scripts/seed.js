629806cc791c440e23c58350355770b9e90209ff
David <davidp@ntestinc.com>
2024-08-08 18:16:32 +0200

    testing datbase

--- alssandro/src/scripts/seed.js
+++ alssandro/src/scripts/seed.js
@@ -1,190 +1,172 @@
-const { Client } = require('pg');
-async function seedProducts(client) {
-  const products = [
-    {
-      name: 'Topping',
-      price: 0.5,
-      image_url: '../../public/images/topping.png',
-    },
-    {
-      name: 'Bola extra',
-      price: 1.5,
-      image_url: '../../public/images/bola_extra.png',
-    },
-    {
-      name: 'Cucurucho Mediano',
-      price: 3.7,
-      image_url: '../../public/images/cucurucho_mediano.png',
-    },
-    {
-      name: 'Cucurucho Grande',
-      price: 4.5,
-      image_url: '../../public/images/cucurucho_grande.png',
-    },
-    {
-      name: 'Cucurucho Solo',
-      price: 0.5,
-      image_url: '../../public/images/cucurucho_solo.png',
-    },
-    {
-      name: 'Cucurucho Chocolate',
-      price: 4.0,
-      image_url: '../../public/images/cucurucho_chocolate_helado.png',
-    },
-    {
-      name: 'Tarrina Pequeña',
-      price: 3.0,
-      image_url: '../../public/images/Tarrina_pequeña.png',
-    },
-    {
-      name: 'Tarrina Mediana',
-      price: 4.0,
-      image_url: '../../public/images/Tarrina_medina.png',
-    },
-    {
-      name: 'Tarrina Grande',
-      price: 5.0,
-      image_url: '../../public/images/Tarrina_grande.png',
-    },
-    {
-      name: 'Crepes 1',
-      price: 4.5,
-      image_url: '../../public/images/crepe_1.png',
-    },
-    {
-      name: 'Crepes 2',
-      price: 5.0,
-      image_url: '../../public/images/crepe_2.png',
-    },
-    {
-      name: 'Crepes 3',
-      price: 5.5,
-      image_url: '../../public/images/crepe_3.png',
-    },
-    {
-      name: 'Waffles Bubble 1',
-      price: 4.5,
-      image_url: '../../public/images/waffes_buble_2.jpg',
-    },
-    {
-      name: 'Waffles Bubble 2',
-      price: 5.0,
-      image_url: '../../public/images/waffes_buble_3.jpg',
-    },
-    {
-      name: 'Waffles Bubble 3',
-      price: 5.5,
-      image_url: '../../public/images/waffes_buble_4.jpg',
-    },
-    {
-      name: 'Gofres 1',
-      price: 4.0,
-      image_url: '../../public/images/gofre_2.png',
-    },
-    {
-      name: 'Gofres 2',
-      price: 4.5,
-      image_url: '../../public/images/gofre_3.png',
-    },
-    {
-      name: 'Gofres 3',
-      price: 5.0,
-      image_url: '../../public/images/gofre_4.png',
-    },
-    {
-      name: 'Smoothies',
-      price: 4.5,
-      image_url: '../../public/images/smoothies.png',
-    },
-    {
-      name: 'Batido Helado',
-      price: 4.5,
-      image_url: '../../public/images/batido_helado.png',
-    },
-    {
-      name: 'Helado Litro',
-      price: 17.0,
-      image_url: '../../public/images/helado_litro.png',
-    },
-    {
-      name: 'Helado Medio Litro',
-      price: 10.0,
-      image_url: '../../public/images/Helado_medio_litro.png',
-    },
-    {
-      name: 'Agua',
-      price: 1.2,
-      image_url: '../../public/images/botella-de-agua.png',
-    },
-    {
-      name: 'Refresco',
-      price: 1.8,
-      image_url: '../../public/images/refresco.png',
-    },
-  ];
+const { db } = require('@vercel/postgres');
+const {
+  invoices,
+  customers,
+  revenue,
+  users,
+} = require('../app/lib/placeholder-data.js');
+const bcrypt = require('bcrypt');
 
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS products (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      price DECIMAL(10, 2) NOT NULL,
-      image_url VARCHAR(255) NOT NULL
-    );
-  `);
+async function seedUsers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    `;
 
-  for (const product of products) {
-    await client.query(
-      `INSERT INTO products (name, price, image_url) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING;`,
-      [product.name, product.price, product.image_url]
+    console.log(`Created "users" table`);
+
+    // Insert data into the "users" table
+    const insertedUsers = await Promise.all(
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return client.sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      })
     );
+
+    console.log(`Seeded ${insertedUsers.length} users`);
+
+    return {
+      createTable,
+      users: insertedUsers,
+    };
+  } catch (error) {
+    console.error('Error seeding users:', error);
+    throw error;
   }
-
-  console.log('Seeded products table');
 }
 
-async function seedSales(client) {
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS sales (
-      id SERIAL PRIMARY KEY,
-      sale_date TIMESTAMP NOT NULL DEFAULT NOW(),
-      total DECIMAL(10, 2) NOT NULL,
-      payment_method VARCHAR(20) NOT NULL CHECK (payment_method IN ('efectivo', 'tarjeta'))
-    );
-  `);
+async function seedInvoices(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  console.log('Created sales table');
+    // Create the "invoices" table if it doesn't exist
+    const createTable = await client.sql`
+    CREATE TABLE IF NOT EXISTS invoices (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    customer_id UUID NOT NULL,
+    amount INT NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    date DATE NOT NULL
+  );
+`;
+
+    console.log(`Created "invoices" table`);
+
+    // Insert data into the "invoices" table
+    const insertedInvoices = await Promise.all(
+      invoices.map(
+        (invoice) => client.sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+        ON CONFLICT (id) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedInvoices.length} invoices`);
+
+    return {
+      createTable,
+      invoices: insertedInvoices,
+    };
+  } catch (error) {
+    console.error('Error seeding invoices:', error);
+    throw error;
+  }
 }
 
-async function seedSaleItems(client) {
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS sale_items (
-      id SERIAL PRIMARY KEY,
-      sale_id INT NOT NULL,
-      product_id INT NOT NULL,
-      quantity INT NOT NULL,
-      price DECIMAL(10, 2) NOT NULL,
-      FOREIGN KEY (sale_id) REFERENCES sales(id) ON DELETE CASCADE,
-      FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
-    );
-  `);
+async function seedCustomers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  console.log('Created sale_items table');
+    // Create the "customers" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS customers (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        image_url VARCHAR(255) NOT NULL
+      );
+    `;
+
+    console.log(`Created "customers" table`);
+
+    // Insert data into the "customers" table
+    const insertedCustomers = await Promise.all(
+      customers.map(
+        (customer) => client.sql`
+        INSERT INTO customers (id, name, email, image_url)
+        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+        ON CONFLICT (id) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedCustomers.length} customers`);
+
+    return {
+      createTable,
+      customers: insertedCustomers,
+    };
+  } catch (error) {
+    console.error('Error seeding customers:', error);
+    throw error;
+  }
+}
+
+async function seedRevenue(client) {
+  try {
+    // Create the "revenue" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS revenue (
+        month VARCHAR(4) NOT NULL UNIQUE,
+        revenue INT NOT NULL
+      );
+    `;
+
+    console.log(`Created "revenue" table`);
+
+    // Insert data into the "revenue" table
+    const insertedRevenue = await Promise.all(
+      revenue.map(
+        (rev) => client.sql`
+        INSERT INTO revenue (month, revenue)
+        VALUES (${rev.month}, ${rev.revenue})
+        ON CONFLICT (month) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedRevenue.length} revenue`);
+
+    return {
+      createTable,
+      revenue: insertedRevenue,
+    };
+  } catch (error) {
+    console.error('Error seeding revenue:', error);
+    throw error;
+  }
 }
 
 async function main() {
-  const client = new Client({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-  });
+  const client = await db.connect();
 
-  await client.connect();
-
-  await seedProducts(client);
-  await seedSales(client);
-  await seedSaleItems(client);
+  await seedUsers(client);
+  await seedCustomers(client);
+  await seedInvoices(client);
+  await seedRevenue(client);
 
   await client.end();
 }
